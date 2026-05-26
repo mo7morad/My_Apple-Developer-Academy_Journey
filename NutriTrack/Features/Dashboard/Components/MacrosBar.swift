a69e9aaef34efde0bd8f75764100c1c584123099
David Paul Ong <davidpaulong@gmail.com>
2026-05-26 12:04:48 +0800

    color extension; bar progress logic

--- NutriTrack/Features/Dashboard/Components/MacrosBar.swift
+++ NutriTrack/Features/Dashboard/Components/MacrosBar.swift
@@ -3,10 +3,12 @@ import SwiftUI
 
 struct MacrosBar: View {
     
-    var barHeight: CGFloat = 150
-    var barWidth: CGFloat = 75
+    var barHeight: CGFloat = 140
+    var barWidth: CGFloat = 70
     var barRadius: CGFloat = 50
     var barBorderWidth: CGFloat = 10
+    
+    var progress: [CGFloat] = [0.9, 0.4, 0.3, 0.5]
     
     var body: some View {
         
@@ -17,81 +19,137 @@ struct MacrosBar: View {
             Spacer()
         }
         
-        HStack(spacing: 17) {
+        HStack(spacing: 23) {
             
-            ZStack {
-                Rectangle()
-                    .fill(Color(hex: "E6E6E6"))
-                    .frame(width: barWidth, height: barHeight)
-                    .cornerRadius(barRadius)
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                                    colors: [Color(hex: "F0B6B6"), Color(hex: "F07B7B")],
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
+            VStack(spacing:5){
+                ZStack(){
+                    Rectangle()
+                        .fill(Color(hex: "E6E6E6"))
+                        .frame(width: barWidth, height: barHeight)
+                        .cornerRadius(barRadius)
+                    
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                        colors: [Color(hex: "F0B6B6"), Color(hex: "F07B7B")],
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                            )
+                        .frame(width: barWidth, height: barHeight * progress[0])
+                        .offset(y: (barHeight - (barHeight-barBorderWidth) * progress[0])/2)
+                        .mask(
+                            Rectangle()
+                                .frame(width: barWidth-barBorderWidth, height: (barHeight-barBorderWidth))
+                                .cornerRadius(barRadius)
                         )
-                    .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
-                    .cornerRadius(barRadius)
+                        
+                    
+//                    Rectangle()
+//                        .fill(
+//                            LinearGradient(
+//                                        colors: [Color(hex: "F0B6B6"), Color(hex: "F07B7B")],
+//                                        startPoint: .top,
+//                                        endPoint: .bottom
+//                                    )
+//                            )
+//                        .frame(width: barWidth-barBorderWidth, height: (barHeight-barBorderWidth) * CGFloat(progress[0]))
+//                        .cornerRadius(barRadius)
+//                        .offset(y: -20)
+                    
+                }
+                Text("Protein")
+                    .font(.system(size: 14))
+                Text("90")
+                    .font(.system(size: 14))
+                Text("120gr")
+                    .font(.system(size: 14))
             }
             
-            ZStack {
-                Rectangle()
-                    .fill(Color(hex: "E6E6E6"))
-                    .frame(width: barWidth, height: barHeight)
-                    .cornerRadius(barRadius)
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                                    colors: [Color(hex: "FFE1B2"), Color(hex: "FFB87A")],
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
-                        )
-                    .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
-                    .cornerRadius(barRadius)
-                Image(systemName: "checkmark.circle")
-                    .foregroundStyle(Color(hex: "BD6205"))
-                    .font(.system(size: 25) .bold())
+            VStack(spacing: 5){
+                ZStack {
+                    Rectangle()
+                        .fill(Color(hex: "E6E6E6"))
+                        .frame(width: barWidth, height: barHeight)
+                        .cornerRadius(barRadius)
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                        colors: [Color(hex: "FFE1B2"), Color(hex: "FFB87A")],
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                            )
+                        .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
+                        .cornerRadius(barRadius)
+                    Image(systemName: "checkmark.circle")
+                        .foregroundStyle(Color(hex: "BD6205"))
+                        .font(.system(size: 25) .bold())
+                }
+                Text("Protein")
+                    .font(.system(size: 14))
+                Text("90")
+                    .font(.system(size: 14))
+                Text("120gr")
+                    .font(.system(size: 14))
             }
-            ZStack {
-                Rectangle()
-                    .fill(Color(hex: "E6E6E6"))
-                    .frame(width: barWidth, height: barHeight)
-                    .cornerRadius(barRadius)
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                                    colors: [Color(hex: "CFE0C6"), Color(hex: "95CC81")],
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
-                        )
-                    .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
-                    .cornerRadius(barRadius)
+            
+            
+            VStack(spacing: 5){
+                ZStack {
+                    Rectangle()
+                        .fill(Color(hex: "E6E6E6"))
+                        .frame(width: barWidth, height: barHeight)
+                        .cornerRadius(barRadius)
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                        colors: [Color(hex: "CFE0C6"), Color(hex: "95CC81")],
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                            )
+                        .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
+                        .cornerRadius(barRadius)
+                }
                 
-
+                Text("Fiber")
+                    .font(.system(size: 14))
+                Text("50")
+                    .font(.system(size: 14))
+                Text("120gr")
+                    .font(.system(size: 14))
+                
             }
-            ZStack {
-                Rectangle()
-                    .fill(Color(hex: "E6E6E6"))
-                    .frame(width: barWidth, height: barHeight)
-                    .cornerRadius(barRadius)
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                                    colors: [Color(hex: "D9D9D9"), Color(hex: "878787")],
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
-                        )
-                    .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
-                    .cornerRadius(barRadius)
+            
+            VStack(spacing: 5){
+                ZStack {
+                    Rectangle()
+                        .fill(Color(hex: "E6E6E6"))
+                        .frame(width: barWidth, height: barHeight)
+                        .cornerRadius(barRadius)
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                        colors: [Color(hex: "D9D9D9"), Color(hex: "878787")],
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                            )
+                        .frame(width: barWidth-barBorderWidth, height: barHeight-barBorderWidth)
+                        .cornerRadius(barRadius)
+                }
+                
+                Text("Carbs")
+                    .font(.system(size: 14))
+                Text("200")
+                    .font(.system(size: 14))
+                Text("200gr")
+                    .font(.system(size: 14))
             }
+            
             
         }
-        .padding(.horizontal, 50)
     }
 }
 
