091f477eb2cb21caef77e37337e1e4794e60f1fd
David Paul Ong <davidpaulong@gmail.com>
2026-06-02 15:33:04 +0800

    polish and number logic

---
+++ NutriTrack/Features/Dashboard/Components/MacroElement.swift
@@ -0,0 +1,18 @@
+//
+//  MacroElement.swift
+//  NutriTrack
+//
+//  Created by David Paul Ong on 02/06/26.
+//
+
+import SwiftUI
+
+struct MacroElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MacroElement()
+}
