3d7142b5c99761d2023044fb3322dc14ee5dc307
Moe <dronaessam@gmail.com>
2026-07-06 10:42:24 +0800

    preparing for adding the color extractor, fixed the dark mode issue, and photos picker now is more simple

--- Project/Distill/Features/Home/HomeView.swift
+++ Project/Distill/Features/Home/HomeView.swift
@@ -11,46 +11,47 @@ struct HomeView: View {
     var body: some View {
 
         NavigationStack {
-
+            
             VStack {
-
+                
                 // MARK: - Header
-
+                
                 Button("Start Painting") {
                     showPhotoPicker = true
                 }
                 .font(.title3.weight(.medium))
                 .buttonStyle(.borderedProminent)
                 .tint(.primary)
+                .foregroundStyle(Color(uiColor: .systemBackground))
                 .controlSize(.large)
-
+                
                 Spacer()
-
+                
                 Divider()
-
+                
                 // MARK: - Painting Grid
-
+                
             }
             .navigationTitle("Distill")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Select") {
-
+                        
                     }
                 }
-
+                
                 ToolbarSpacer(.fixed, placement: .topBarTrailing)
-
+                
                 ToolbarItemGroup(placement: .topBarTrailing) {
                     Button {
-
+                        
                     } label: {
                         Image(systemName: "translate")
                     }
                     .accessibilityLabel("Translate")
-
+                    
                     Button {
-
+                        
                     } label: {
                         Image(systemName: "info.circle")
                     }
@@ -62,18 +63,33 @@ struct HomeView: View {
                 selection: $selectedPhotoItem,
                 matching: .images
             )
-            // Just a dummy Task for now, it discards whatever the user choose.
+            .photosPickerDisabledCapabilities(.collectionNavigation)
+            
+            
+            // MARK: - Extracting the dominant colors.
+            
             .onChange(of: selectedPhotoItem) { _, newItem in
                 guard let newItem else { return }
-
+                
                 Task {
+                    // Loading and casting the PhotoPicker into Image so we can resize it.
                     if let data = try? await newItem.loadTransferable(type: Data.self),
-                       let uiImage = UIImage(data: data) {
-                        print("Picked image: \(uiImage.size)")
+                       let originalImage = UIImage(data: data) {
+                        
+                        print("Original size: \(originalImage.size)")
+                        
+                        // Image resizing
+                        let targetSize = CGSize(width: 100, height: 100)
+                        if let resizedImage = originalImage.resize(to: targetSize) {
+                            print("Resized image: \(resizedImage.size)")
+                            
+                            // NEXT STEP: Extracting Colors.
+                        }
+                        
                     } else {
                         print("Failed to load selected photo")
                     }
-
+                    
                     selectedPhotoItem = nil
                 }
             }
